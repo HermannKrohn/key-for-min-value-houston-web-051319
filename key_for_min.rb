2776cc53841886edca2987363c02a5772a0e7784
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-05 04:00:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,5 +7,12 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if smallestValue == nil 
-      
+      keySmallestValue = key 
+      smallestValue = value
+    elsif value < smallestValue
+      smallestValue = value
+      keySmallestValue = key
+    end
+  end
+  keySmallestValue
 end
